eb0c4274a49d918de2e1fdc05695e4a808609e1a
Bryan Matthew <bryanmatthew206@gmail.com>
2023-09-30 20:47:55 +0800

    add popup

--- src/components/ui/UploadDialog.jsx
+++ src/components/ui/UploadDialog.jsx
@@ -1,13 +1,23 @@
+"use client";
+
 import React, { useRef, useState } from "react";
 import * as Dialog from "@radix-ui/react-dialog";
 import { Cross2Icon } from "@radix-ui/react-icons";
 import axios from "axios";
+import { BsCloudUploadFill } from "react-icons/bs";
+import toast from "react-hot-toast";
+const STATUS_IDLE = "idling";
+const STATUS_UPLOADING = "uploading";
 
-const DialogDemo = () => {
+const UploadDialog = () => {
     const [files, setFiles] = useState();
     const selectFileBtn = useRef(null);
+    const [numberOfFiles, setNumberOfFiles] = useState(0);
+    const [status, setStatus] = React.useState(STATUS_IDLE);
 
     const uploadFiles = (data) => {
+        setStatus(STATUS_UPLOADING);
+        // dummy doang
         axios
             .post("/upload", data, {
                 headers: {
@@ -15,11 +25,25 @@ const DialogDemo = () => {
                 },
             })
             .then(() => {
-                console.log("success bro");
+                toast.success("Successfully uploaded!");
             })
             .catch((err) => {
-                console.log(err);
+                toast.error(err);
+            })
+            .finally(() => {
+                setStatus(STATUS_IDLE);
+                resetInput();
             });
+    };
+
+    const resetInput = () => {
+        setFiles([]);
+        setNumberOfFiles(0);
+    };
+
+    const handleFileInputChange = (e) => {
+        setFiles(e.target.files);
+        setNumberOfFiles(e.target.files.length);
     };
 
     const packFiles = (files) => {
@@ -41,30 +65,38 @@ const DialogDemo = () => {
     return (
         <Dialog.Root>
             <Dialog.Trigger asChild>
-                <button className="bg-white text-violet-500 border rounded-md px-4 py-2 shadow-lg">
+                <button
+                    className="bg-white text-violet-500 border rounded-md px-4 py-2 shadow-lg"
+                    onClick={resetInput}
+                >
                     Add Resume
                 </button>
             </Dialog.Trigger>
             <Dialog.Portal>
-                <Dialog.Overlay className="fixed inset-0 opacity-30 bg-black animate-overlay-show " />
+                <Dialog.Overlay className="fixed inset-0 opacity-50 bg-black animate-overlay-show " />
                 <Dialog.Content className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-md shadow-lg max-w-[450px] max-h-[85vh] p-6 animate-content-show focus:outline-none w-full">
                     <Dialog.Title className="text-black font-semibold">
                         Add Resume
                     </Dialog.Title>
                     <form
-                        className="mt-5 flex justify-center ali h-[200px] w-full cursor-pointer items-center"
+                        className="mt-5 flex flex-col justify-center h-[200px] w-full cursor-pointer items-center"
                         style={{
-                            border: "2px dashed #1475dc",
+                            border: "2px dashed #8b5cf6",
                             borderRadius: "5px",
                         }}
                         onClick={() => selectFileBtn.current.click()}
                     >
+                        <BsCloudUploadFill
+                            color="#8b5cf6"
+                            className="w-[150px] h-[150px]"
+                        />
+                        <p className="text-black">
+                            {numberOfFiles} files are selected
+                        </p>
                         <input
                             type="file"
                             name="file"
-                            onChange={(e) => {
-                                setFiles(e.target.files);
-                            }}
+                            onChange={handleFileInputChange}
                             accept=".pdf"
                             multiple
                             ref={selectFileBtn}
@@ -72,14 +104,13 @@ const DialogDemo = () => {
                         />
                     </form>
                     <div className="flex justify-end">
-                        <Dialog.Close asChild>
-                            <button
-                                className="text-green-900 border rounded-md px-4 py-2 hover:bg-green-50"
-                                onClick={handleUploadClick}
-                            >
-                                Upload
-                            </button>
-                        </Dialog.Close>
+                        <button
+                            className="text-green-900 border rounded-md px-4 py-2 hover:bg-green-50 mt-3"
+                            onClick={handleUploadClick}
+                            disabled={status === STATUS_UPLOADING}
+                        >
+                            Upload
+                        </button>
                     </div>
                     <Dialog.Close asChild>
                         <button className="text-violet-500 absolute top-2 right-2 w-7 h-7 p-0.5 bg-white rounded-full">
@@ -92,4 +123,4 @@ const DialogDemo = () => {
     );
 };
 
-export default DialogDemo;
+export default UploadDialog;
--- tailwind.config.js
+++ tailwind.config.js
@@ -67,7 +67,7 @@ module.exports = {
         },
         "overlayShow": {
           from: { opacity: 0 },
-          to: { opacity: 0.3 },
+          to: { opacity: 0.5 },
         },
         "contentShow": {
           from: {
