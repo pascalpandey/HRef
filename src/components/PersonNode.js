0899eac3af25a10ba9d9878c24b28b14a006557b
Bryan Matthew <bryanmatthew206@gmail.com>
2023-10-01 12:24:52 +0800

    populate data of employee and applicants in popup

--- src/components/PersonNode.js
+++ src/components/PersonNode.js
@@ -2,20 +2,29 @@
 
 import * as HoverCard from "@radix-ui/react-hover-card";
 
-export default function PersonNode() {
+export default function PersonNode({ data, viewEmployee }) {
   return (
     <HoverCard.Root>
       <HoverCard.Trigger asChild>
-        <button
-          className="rounded-full bg-red-400 "
-          style={{ width: `${48}px`, height: `${48}px` }}
-        />
+        <a href={data?.resumeLink} target="_blank">
+          <button
+            className="rounded-full bg-red-400 "
+            style={{ width: `${48}px`, height: `${48}px`, backgroundColor: data?.color }}
+          />
+        </a>
       </HoverCard.Trigger>
       <HoverCard.Portal>
         <HoverCard.Content
-          className="w-64 h-48 data-[side=bottom]:animate-slideUpAndFade data-[side=right]:animate-slideLeftAndFade data-[side=left]:animate-slideRightAndFade data-[side=top]:animate-slideDownAndFade rounded-md bg-white p-5 shadow-lg data-[state=open]:transition-all"
+          className="w-64 min-h-32 data-[side=bottom]:animate-slideUpAndFade data-[side=right]:animate-slideLeftAndFade data-[side=left]:animate-slideRightAndFade data-[side=top]:animate-slideDownAndFade rounded-md bg-white p-5 shadow-lg data-[state=open]:transition-all text-black"
           sideOffset={5}
-        ></HoverCard.Content>
+        >
+          <p>
+            <span className="font-bold">{viewEmployee ? "Employee Id" : "Applicant Id"}</span>: {data?.name}
+          </p>
+
+          <p><span className="font-bold">Keywords </span> {data?.keywords.join(", ")}</p>
+          <p><span className="font-bold">Score </span>: {data?.score}</p>
+        </HoverCard.Content>
       </HoverCard.Portal>
     </HoverCard.Root>
   );
